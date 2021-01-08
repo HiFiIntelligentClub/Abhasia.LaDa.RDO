89eef5176c303ff9b8d875a62a1116f5e5aa14e4
Andrey Chekmarev <assminog@gmail.com>
2021-01-08 13:49:19 +0300

    Updated

--- Abhasia.LaDa.RDO1.php
+++ Abhasia.LaDa.RDO1.php
@@ -4,7 +4,6 @@
 require_once('/home/EDRO.SOT/System/0.Functions/0.strNDigit.php');
 require_once('/home/EDRO.SetOfTools/System/2.VectorKIIM/0.KIIM.php');
 
-
 //$socket = stream_socket_server("tcp://hic:8081", $errno, $errstr);
 $objKIIM=KIIM::objStart(false , array(
 	'_strClass'		=>'Socket',
@@ -12,7 +11,7 @@ $objKIIM=KIIM::objStart(false , array(
 	'_strMessage'		=>'',
 	'_strVectorPoint'	=>'',
 	));
-	$socket = stream_socket_server("tcp://127.0.0.1:8080", $errno, $errstr);
+	$socket = stream_socket_server("tcp://192.168.1.198:80", $errno, $errstr);
 	KIIM::objFinish($objKIIM, array(
 		'_strClass'		=>'Socket',
 		'_strMethod'		=>'Start',
@@ -20,7 +19,6 @@ $objKIIM=KIIM::objStart(false , array(
 		'_strVectorPoint'	=>'',
 		));
 print_r($objKIIM);
-
 //socket_bind($socket, 'hic', 8081);
 while ($connect = stream_socket_accept($socket, -1))
 	{
@@ -30,12 +28,11 @@ while ($connect = stream_socket_accept($socket, -1))
 		'_strMessage'		=>'',
 		'_strVectorPoint'	=>'',
 		));
-
 	$strBuffer	='';
 	$strContentType	='Content-Type: text/html';
 	//echo $_strBuf=socket_read($socket, 256, PHP_NORMAL_READ);
 	//echo "Connect\n";
-	$str=fread($connect, 512);
+	$str		=fread($connect, 512);
 	echo date('Y-m-d H:i:s')."\n\n";
 	echo 'vvvvvvvvvvvvvvvvvvvvvvvv-fRead-Connect-vvvvvvvvvvvvvvvvvv'."\n";
 	echo$str."\n";
@@ -61,39 +58,38 @@ while ($connect = stream_socket_accept($socket, -1))
 			echo '^^^^^^^^^^^^^^^^^^^^^^^^-Run-With-Request-^^^^^^^^^^^^^^^^^^'."\n";
 			exec('/home/HiFiIntelligentClub.Ru/run.php '.$strRequest, $arrOut);
 			unset($strRequest);
-			//echo $strBuffer		=file_get_contents('/home/HiFiIntelligentClub.Ru/index.php ');
-			$strOut='';
-			foreach($arrOut as $strOut)
-				{
-				$strBuffer	.=$strOut;
-				}
-			unset($arrOut);
-			unset($strOut);
-			$strBuffer	=str_replace(array("\r", "\n", "\t"), "", $strBuffer);
-			file_put_contents('/home/EDRO.SetOfTools/DjService/Abhasia_debug.txt', $strBuffer);
-			$strBuffer	='<AbhasiaServeWarning style="display:block;overflow:hidden;font-size:x-large;height:20px;line-height:19px;color:red;">Development version. For stable, visit <a href="http://HiFiIntelligentClub.COM">HiFiIntelligentClub.COM</a></AbhasiaServeWarning>'.$strBuffer;
-			$strBufferLen	=strlen($strBuffer);
-			//echo $strBuffer;
-			fwrite($connect, "HTTP/1.1 200 OK\r\n".$strContentType."\r\nServer-name: Abhasia LaDa.Rdo\r\nContent-Length:".$strBufferLen."\r\nConnection: close\r\n\r\n".$strBuffer);
-			fclose($connect);
-			unset($strBuffer);
-			}
-		elseif(isset($arrRequest[1])&&$arrRequest[1]=="/favicon.ico")
-			{
-			$faviconBin	=readfile('/home/HiFiIntelligentClub.Ru/favicon.png');
-			fwrite($connect, "HTTP/1.1 200 OK\r\nContent-Type: image/png\r\nServer-name: Abhasia LaDa.Rdo\r\nContent-Length:".strlen($faviconBin)."\r\nConnection: close\r\n\r\n".$faviconBin);
-			unset($faviconBin);
-			}
-		else
-			{
-			}
-		}
-	//echo "\n";
+	    //echo $strBuffer		=file_get_contents('/home/HiFiIntelligentClub.Ru/index.php ');
+	    $strOut='';
+	    foreach($arrOut as $strOut)
+	    	{
+	    	$strBuffer	.=$strOut;
+	    	}
+	    unset($arrOut);
+	    unset($strOut);
+	    $strBuffer										=str_replace(array("\r", "\n", "\t"), "", $strBuffer);
+	    file_put_contents('/home/EDRO.SetOfTools/DjService/Abhasia_debug.txt', $strBuffer);
+	    $strBuffer	='<AbhasiaServeWarning style="display:block;overflow:hidden;font-size:x-large;height:20px;line-height:19px;color:red;">Development version. For stable, visit <a href="http://HiFiIntelligentClub.COM">HiFiIntelligentClub.COM</a></AbhasiaServeWarning>'.$strBuffer;
+	    $strBufferLen	=strlen($strBuffer);
+	    //echo $strBuffer;
+	    fwrite($connect, "HTTP/1.1 200 OK\r\n".$strContentType."\r\nServer-name: Abhasia LaDa.Rdo\r\nContent-Length:".$strBufferLen."\r\nConnection: close\r\n\r\n".$strBuffer);
+	    fclose($connect);
+	    unset($strBuffer);
+	    }
+	elseif(isset($arrRequest[1])&&$arrRequest[1]=="/favicon.ico")
+	    {
+	    $faviconBin	=readfile('/home/HiFiIntelligentClub.Ru/favicon.png');
+	    fwrite($connect, "HTTP/1.1 200 OK\r\nContent-Type: image/png\r\nServer-name: Abhasia LaDa.Rdo\r\nContent-Length:".strlen($faviconBin)."\r\nConnection: close\r\n\r\n".$faviconBin);
+	    unset($faviconBin);
+	    }
+	else
+	    {
+	    }
+	}
 	KIIM::objFinish($objKIIM, array(
 		'_strClass'		=>'Socket',
 		'_strMethod'		=>'Iteration',
 		'_strMessage'		=>'',
 		'_strVectorPoint'	=>'',
 		));
-	}
+    }
 ?>
