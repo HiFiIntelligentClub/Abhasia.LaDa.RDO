416ee6bd9268c8eee125f7c9f4819a343a6d6aff
Andrey Chekmarev <assminog@gmail.com>
2021-01-20 18:18:33 +0300

    в работе

--- Abhasia.LaDa.RDO1.php
+++ Abhasia.LaDa.RDO1.php
@@ -1,7 +1,6 @@
 #!/usr/bin/php
 <?php
 // © A.A.CheckMaRev assminog@gmail.com tubmulur@yandex.ru first sucsessfull run at Birthday of Gersan Gioev 26.11.2020
-session_start();
 $_SERVER['REQUEST_URI']		='/';
 $_SERVER['SERVER_NAME']		='HiFiIntelligentClub.Ru';
 $_SERVER['HTTP_USER_AGENT']	='Abhasia LaDa.RDo';
@@ -13,25 +12,53 @@ require_once('/home/EDRO.SetOfTools/System/2.VectorKIIM/0.KIIM.php');
 //require_once('/home/EDRO.SetOfTools/System/5.Templates/0.strKIIM.Template.php');
 require_once('/home/EDRO.SetOfTools/System/3.Buffer/1.EDRO_Buffering.php');
 
-				$objKIIM=KIIM::objStart(false , array('_strClass'=>'Socket','_strMethod'=>'Start','_strMessage'	=>'stream_socket_server','_strVectorPoint'=>'',));
-$socket = stream_socket_server("tcp://hifiintelligentclub.ru:80", $errno, $errstr);
-				KIIM::objFinish($objKIIM, array('_strClass'=>'Socket','_strMethod'=>'Start','_strMessage'=>'stream_socket_server','_strVectorPoint'=>'',));
+function рОрганизацияПриёмникаЗапросовСлушателя()
+	{
+	$objKIIM=KIIM::objStart(false , array('_strClass'=>'Socket','_strMethod'=>'Start','_strMessage'	=>'stream_socket_server','_strVectorPoint'=>'',));
+
+	$рПриёмникЗапросовСлушателя	=stream_socket_server("tcp://hifiintelligentclub.ru:80", $errno, $errstr);
+
+	KIIM::objFinish($objKIIM, array('_strClass'=>'Socket','_strMethod'=>'Start','_strMessage'=>'stream_socket_server','_strVectorPoint'=>'',));
+	return $рПриёмникЗапросовСлушателя;
+	}
+function мЗапросИзБраузераСлушателя($_lnConnect)
+	{
+	$strConnect		=fread($_lnConnect, 512);
+	$arrHeaders		=explode("\n", $strConnect);
+	return $arrHeaders;
+	}
+function arrGetHeaders($_мЗапрос)
+	{
+	$_мЗапрос	=array();
+	foreach($_мЗапрос as $сЗапрос)
+		{
+		$мЗапрос[]	=explode(":" , $сЗапрос);
+		}
+	return $мЗапрос;
+	}
+function мЗаголовкиЗапроса($_мЗаголовки)
+	{
+	$мЗаголовки	=explode(" ", $_мЗаголовки[0]);
+	return $мЗаголовки;
+	}
+
+$рПриёмник	=рОрганизацияПриёмникаЗапросовСлушателя();
 
 //$strBufferServerNotice		='<h1>Сервер Абхазия.LaDa,RDo</h1><AbhasiaServeWarning style="display:block;overflow:hidden;font-size:x-large;height:20px;line-height:19px;color:red;">Development version. For stable, visit <a href="http://HiFiIntelligentClub.COM">HiFiIntelligentClub.COM</a></AbhasiaServeWarning>';
-while ($connect = stream_socket_accept($socket, -1))
+while ($рПриём = stream_socket_accept($рПриёмник, -1))
 	{$objKIIM=KIIM::objStart($objKIIM, array('_strClass'=>'Socket','_strMethod'=>'Start','_strMessage'=>'stream_socket_accept','_strVectorPoint'=>'',));
 
-	$strConnect		=fread($connect, 512);
+	$мЗаголовкиСлушателя	=мЗапросИзБраузераСлушателя($рПриём);
 
-	$arrHeaders		=explode("\n", $strConnect);
-	if(isset($arrHeaders[0]))
+	if(isset($мЗаголовкиСлушателя[0]))
 		{
 		$arrRequest		=explode(" ", $arrHeaders[0]);
+
 		if(isset($arrRequest[1])&&$arrRequest[1]!="/favicon.ico")
 			{
 			$strContentType		='Content-Type: text/html';
 			$objEDRO		=new Event($objKIIM);
-			require_once$objEDRO->arrDesign['strTemplate'];
+			require_once		$objEDRO->arrDesign['strTemplate'];
 			$strBuffer		=str_replace(array("\r\n\r\n", "\n\n"), "", $str);
 			if($objEDRO->arrEvent['bIzDynamic'])
 				{
