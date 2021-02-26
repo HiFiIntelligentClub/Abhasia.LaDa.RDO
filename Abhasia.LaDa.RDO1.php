da98d05e254eb29c3d836bfd3e9cda1a13340c75
Andrey Chekmarev <assminog@gmail.com>
2021-02-26 15:58:54 +0300

    EDRO

--- Abhasia.LaDa.RDO1.php
+++ Abhasia.LaDa.RDO1.php
@@ -3,6 +3,7 @@
 // © A.A.CheckMaRev assminog@gmail.com tubmulur@yandex.ru first sucsessfull run at Birthday of Gersan Gioev 26.11.2020
 //р=Ресурс	сп=СтрокаПакет(Пакет для вебсервера)
 set_time_limit(0);
+file_put_contents('x.txt',"\n=====\n".'	Start:		'.date("Y-m-d H:i:s")."\n", FILE_APPEND);
 require('/home/EDRO.SetOfTools/System/0.Functions/0.strNDigit.php');
 require('/home/EDRO.SetOfTools/System/1.Reporter/0.ReportError.php');
 require('/home/EDRO.SetOfTools/System/1.Reporter/1.Report.php');
@@ -10,12 +11,16 @@ require('/home/EDRO.SetOfTools/System/0.Functions/2.Dyn.php');
 require('/home/EDRO.SetOfTools/System/2.VectorKIIM/0.KIIM.php');
 require('/home/EDRO.SetOfTools/System/3.Buffer/1.EDRO_Buffering.php');
 require('/home/EDRO.SetOfTools/DjService/Abhasia.lib.php');
-
-
+/*Буфферинг */
+$strJPG			=file_get_contents('/home/HiFiIntelligentClub.Ru/Hfic_Samin.jpg');
+$robotsTxt		=file_get_contents('/home/HiFiIntelligentClub.Ru/robots.txt');
+/*Буфферинг */
 $рПриёмник	=рОрганизацияПриёмникаЗапросовСлушателя();
+file_put_contents('x.txt', '	FinishLoad:		'.date("Y-m-d H:i:s")."\n", FILE_APPEND);
 
 while ($рПередача = stream_socket_accept($рПриёмник, -1))
 	{
+	file_put_contents('x.txt', '	StartLIVE:			'.date("Y-m-d H:i:s")."\n", FILE_APPEND);
 	//$objKIIM=KIIM::objStart($objKIIM, array('_strClass'=>'Socket','_strMethod'=>'Start','_strMessage'=>'stream_socket_accept','_strVectorPoint'=>'',));
 	//$_SERVER['REQUEST_URI']		='/';
 	$мЗаголовкиСлушателя		=мЧтениеЗапросаИзБраузераСлушателя($рПередача);
@@ -33,19 +38,30 @@ while ($рПередача = stream_socket_accept($рПриёмник, -1))
 		{
 		$_SERVER['strListener']		='';
 		}
-		//socket_getpeername($рПриёмник, $strI, $intI);
-		//echo  $strI;
-		//echo $intI;
-		//echo "\n";
 	if(isset($мЗаголовкиСлушателя[0]))
 		{
+		file_put_contents('x.txt', "\n".$_SERVER['strListener'].'	AfterReadListener:				'.date("Y-m-d H:i:s")."\n", FILE_APPEND);
 		$мЗаголовки			=мЗаголовкиЗапроса($мЗаголовкиСлушателя);
+		$сРасширение			=mb_strtolower(сКонцДоСимвола($мЗаголовки[1], '.'));
 		if(
 			isset($мЗаголовки[1])
 			&&$мЗаголовки[1]!="/favicon.ico"
 			&&$мЗаголовки[1]!="/robots.txt"
+			&&($сРасширение=="jpg")
 			)
 			{
+			fwrite($рПередача, сПостроитьПакетДанныхЛоготипКартинка($strJPG));
+			fclose($рПередача);
+			unset($спДляОтправкиСлушателю);	
+			}
+		elseif(
+			isset($мЗаголовки[1])
+			&&$мЗаголовки[1]!="/favicon.ico"
+			&&$мЗаголовки[1]!="/robots.txt"
+			&&($сРасширение!="jpg")
+			)
+			{
+			file_put_contents('x.txt', $_SERVER['strListener'].'	Before push page:				'.date("Y-m-d H:i:s")."\n", FILE_APPEND);
 			$_SERVER['REQUEST_URI']		=$мЗаголовки[1];
 			$_SERVER['REMOTE_ADDR']		='<ifEN>Temporary disabled</ifEN><ifRU>Временно отключено</ifRU>';
 			$спДляОтправкиСлушателю		=сПостроитьПакетДанных();
@@ -53,27 +69,35 @@ while ($рПередача = stream_socket_accept($рПриёмник, -1))
 			fclose($рПередача);
 			file_put_contents('/home/EDRO.SetOfTools/DjService/Abhasia_debug.txt', $спДляОтправкиСлушателю);
 			unset($спДляОтправкиСлушателю);
+			file_put_contents('x.txt', $_SERVER['strListener'].'	After push page:				'.date("Y-m-d H:i:s")."\n", FILE_APPEND);
 			}
 		elseif(
 			isset($мЗаголовки[1])
 			&&$мЗаголовки[1]=="/favicon.ico"
 			&&$мЗаголовки[1]!="/robots.txt"
+			&&($сРасширение!="jpg")
 			)
 			{
+			file_put_contents('x.txt', '	Before push fav:'.date("Y-m-d H:i:s")."\n", FILE_APPEND);
 			fwrite($рПередача, сПостроитьПакетДанныхЛоготипИконка(), strlen(сПостроитьПакетДанныхЛоготипИконка()));
 			fclose($рПередача);
+			file_put_contents('x.txt', '	After push fav:'.date("Y-m-d H:i:s")."\n", FILE_APPEND);
 			}
 		elseif(
 			isset($мЗаголовки[1])
 			&&$мЗаголовки[1]!="/favicon.ico"
 			&&$мЗаголовки[1]=="/robots.txt"
+			&&($сРасширение!="jpg")
 			)
 			{
+			file_put_contents('x.txt', $_SERVER['strListener'].'	Before push robots:				'.date("Y-m-d H:i:s")."\n", FILE_APPEND);
 			fwrite($рПередача, сПостроитьПакетДанныхРоботТхт(), strlen(сПостроитьПакетДанныхРоботТхт()));
 			fclose($рПередача);
+			file_put_contents('x.txt', $_SERVER['strListener'].'	After push robots:				'.date("Y-m-d H:i:s")."\n", FILE_APPEND);
 			}
 		else
 			{
+			
 			}
 		}
 	}
